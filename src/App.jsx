9888ee22a5b4114707f316e69e81b100bcf0aef5
arun1111j <justarun0210@gmail.com>
2025-03-24 08:58:03 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,33 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Create from "./components/Create";
 import Edit from "./components/Edit";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import NavScrollExample from "./components/NavScrollExample";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";  // Bootstrap JavaScript
-import "./App.css";  // Import the CSS file
-
+import "bootstrap/dist/js/bootstrap.bundle.min"; // Bootstrap JavaScript
+import "./App.css"; // Import the CSS file
 
 function App() {
   return (
-    <>
-      {<NavScrollExample/>}
-    
+    <Router>
+      <NavScrollExample /> {/* Navbar inside Router */}
+
       <div className="app">
         <h1 className="title">Saveetha Placement Cell</h1>
-        <h3>Students portal</h3>
+        <h3>Students Portal</h3>
       </div>
-      
-      <BrowserRouter>
-        <Routes>
-          <Route path="/home" element={<Home/>} />
-          <Route path="/create" element={<Create/>} />
-          <Route path="/edit" element={<Edit/>} />
-          <Route path="/login" element={<Login/>} />
-          {/* <Route path="/" element={<NavScrollExample/>} /> */}
-        </Routes>
-      </BrowserRouter>
-    </>
+
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Default route */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/edit" element={<Edit />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
