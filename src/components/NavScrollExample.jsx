6acccd71ea942ee13605d377955e7cbea98ea449
arun1111j <justarun0210@gmail.com>
2025-03-24 09:07:28 +0530

    commit-nav

--- src/components/NavScrollExample.jsx
+++ src/components/NavScrollExample.jsx
@@ -1,25 +1,23 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from "react-router-dom"; // Import Link for navigation
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import "./NavScrollExample.css";
+
 function NavScrollExample() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Navbar scroll</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          />
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/create">Create</Nav.Link>
-            <Nav.Link href="/edit">Update</Nav.Link>
+          <Nav className="me-auto my-2 my-lg-0" navbarScroll>
+            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+            <Nav.Link as={Link} to="/create">Create</Nav.Link>
+            <Nav.Link as={Link} to="/edit">Update</Nav.Link>
+          </Nav>
           <Form className="d-flex">
             <Form.Control
               type="search"
